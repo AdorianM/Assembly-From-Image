16f36216a0911f0c2c0ad8ea1ad09fd9aab9795b
Adorian Mititean <Adorian.Mititean@student.utcluj.ro>
2022-06-03 22:31:54 +0300

    Treat exceptions

--- png2asm.py
+++ png2asm.py
@@ -1,6 +1,12 @@
-from PIL import Image
+from msilib.schema import Error
+from PIL import Image, UnidentifiedImageError
 from sys import exit, argv
 from os import path
+
+def print_and_quit(message):
+    print(message)
+    input("Press anything to quit...")
+    exit()
 
 def path_to_basename(file_path):
     return path.splitext(file_path)[0]
@@ -9,16 +15,19 @@ def write_hexels_to_file(fileName, hexels, width, height):
     outputName = fileName + '.inc'
     variableName = "var" + fileName
     f = open(outputName, "w")
-    f.write(variableName + " DD ")
-
-    for y in range(height):
-        if y != 0:
-            f.write(" "*len(variableName) + " DD ")
-        for x in range(width - 1):
+    if f:
+        f.write(variableName + " DD ")
+        for y in range(height):
+            if y != 0:
+                f.write(" "*len(variableName) + " DD ")
+            for x in range(width - 1):
+                f.write(hexels[height*x + y])
+                f.write(", ")
             f.write(hexels[height*x + y])
-            f.write(", ")
-        f.write(hexels[height*x + y])
-        f.write("\n")
+            f.write("\n")
+    else:
+        print_and_quit("Error writing to file")
+    
 
 def get_pixels_to_hex(pixels, width, height):
     hexels = []
@@ -27,36 +36,41 @@ def get_pixels_to_hex(pixels, width, height):
         for y in range(height):
             pixel = pixels[x, y]
             hexel = "0"
-            for color in pixel[:3]:
-                hexVal = "{:02x}".format(color)
-                hexel += hexVal
+            try:
+                for color in pixel[:3]:
+                    hexVal = "{:02x}".format(color)
+                    hexel += hexVal
+            except IndexError:
+                print_and_quit("Error: Color index out of bounds. Image is not RGB")
             hexel += 'h'
             hexels.append(hexel)
     return hexels
 
 def img2bytes(imgPath, imgName):
-    im = Image.open(imgPath)
-    pixels = im.load()
-    x = im.size[0]
-    y = im.size[1]
-    hexels = get_pixels_to_hex(pixels, x, y)
-    write_hexels_to_file(imgName, hexels, x, y)
+    try:
+        im = Image.open(imgPath)
+        pixels = im.load()
+        x = im.size[0]
+        y = im.size[1]
+        hexels = get_pixels_to_hex(pixels, x, y)
+        write_hexels_to_file(imgName, hexels, x, y)
+    except FileNotFoundError:
+        print_and_quit("The specified file path is incorrect")
+    except UnidentifiedImageError:
+        print_and_quit("The provided file is either not an image or corrupt")
+    except Exception as e:
+        print_and_quit("An error occurred: " + str(e))
 
 def main():
     if len(argv) > 1:
-
         imgPath = argv[1]
         imgName = path_to_basename(imgPath)
 
         img2bytes(imgPath, imgName)
 
-        print("File " + imgName + " successfully created")
-        input("Press anything to continue...")
-        exit()
+        print_and_quit("File " + imgName + " successfully created")
 
-    print("Drag & drop a file on the executable to produce the assembly data output")
-    input("Press anything to continue...")
-    exit()
+    print_and_quit("Drag & drop a file on the executable to produce the assembly data output")
     
 
 if __name__ == "__main__":
