b5517fb2b3ca15a962130ffc260c7567a4379428
Adorian Mititean <Adorian.Mititean@student.utcluj.ro>
2022-06-11 00:44:12 +0300

    class-based

--- interface.py
+++ interface.py
@@ -1,72 +1,76 @@
-import tkinter
+import tkinter as tk
 from PIL import Image, ImageTk
 
 default_padx = 10
 default_pady = 5
 
-# Create a user interface using tkinter.
-def create_user_interface():
-    # Create a window.
-    root = tkinter.Tk()
-    root.title("Png to Asm Converter")
-    root.geometry("480x260")
-    root.iconbitmap("assets/chef.ico")
+class MainApplication(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.initialize()
 
-    frame = tkinter.Frame(root, borderwidth=14, bg="#EFEFEA")
-    frame.pack(fill="both", expand=True)
+    def initialize(self):
+        self.parent.title("Png to Asm Converter")
+        self.parent.geometry("480x260")
+        self.parent.iconbitmap("assets/chef.ico")
 
-    # Adding content to the frame
-    logo = ImageTk.PhotoImage(Image.open("assets/chef.ico"))
-    label = tkinter.Label(frame, image=logo)
-    label.pack()
+        baseFrame = tk.Frame(self.parent, borderwidth=14, bg="#EFEFEA")
 
-    settingsFrame = tkinter.LabelFrame(frame, text="Settings Panel", bg="#EFEFEA")
-    settingsFrame.pack()
+        self.logo = ImageTk.PhotoImage(Image.open("assets/chef.ico"))
+        self.logoLabel = tk.Label(baseFrame, image=self.logo)
+        self.settingFrame = tk.LabelFrame(baseFrame, text="Settings Panel", bg="#EFEFEA")
+        self.convertButton = tk.Button(baseFrame, text="Convert", command=())
 
-    # Add a button to frame
-    convertButton = tkinter.Button(frame, text="Convert", command=())
-    convertButton.pack(padx=5, pady=5)
+        self.outputSettingsFrame = tk.LabelFrame(self.settingFrame, text="Output Settings", bg="#EFEFEA")
+        self.otherSettingsFrame = tk.LabelFrame(self.settingFrame, text="Other Settings", bg="#EFEFEA")
 
-    # Adding content to the settings Frame
-    outputSettingsFrame = tkinter.LabelFrame(settingsFrame, text="Output", bg="#EFEFEA")
-    outputSettingsFrame.grid(row=0, column=0, padx=default_padx, pady=default_pady)
+        self.outputEntry1 = tk.Frame(self.outputSettingsFrame, bg="#EFEFEA")
+        self.outputLabel1 = tk.Label(self.outputEntry1, text="Output File Name:")
+        self.outputText1 = tk.Text(self.outputEntry1, height=1, width=5)
 
-    otherSettingsFrame = tkinter.LabelFrame(settingsFrame, text="Other", bg="#EFEFEA")
-    otherSettingsFrame.grid(row=0, column=1, padx=default_padx, pady=default_pady)
+        self.outputEntry2 = tk.Frame(self.outputSettingsFrame, bg="#EFEFEA")
+        self.outputLabel2 = tk.Label(self.outputEntry2, text="Output File Name:")
+        self.outputText2 = tk.Text(self.outputEntry2, height=1, width=5)
 
-    # Add content to the output frame
-    outputLabel = tkinter.Label(outputSettingsFrame, text="Var name:")
-    outputLabel.grid(row=0, column=0, padx=default_padx, pady=default_pady)
+        self.outputEntry3 = tk.Frame(self.otherSettingsFrame, bg="#EFEFEA")
+        self.outputLabel3 = tk.Label(self.outputEntry3, text="Output File Name:")
+        self.outputText3 = tk.Text(self.outputEntry3, height=1, width=5)
 
-    outputEntry = tkinter.Text(outputSettingsFrame, width=10, height=1)
-    outputEntry.grid(row=0, column=1, padx=default_padx, pady=default_pady)
+        self.outputEntry4 = tk.Frame(self.otherSettingsFrame, bg="#EFEFEA")
+        self.outputLabel4 = tk.Label(self.outputEntry4, text="Output File Name:")
+        self.outputText4 = tk.Text(self.outputEntry4, height=1, width=5)
 
-    outputLabel = tkinter.Label(outputSettingsFrame, text="Var name:")
-    outputLabel.grid(row=1, column=0, padx=default_padx, pady=default_pady)
+        baseFrame.pack(fill="both", expand=True)
 
-    outputEntry = tkinter.Text(outputSettingsFrame, width=10, height=1)
-    outputEntry.grid(row=1, column=1, padx=default_padx, pady=default_pady)
+        self.logoLabel.pack(side=tk.TOP)
+        self.settingFrame.pack(side=tk.TOP, fill="both", expand=True, padx=default_padx, pady=default_pady)
+        self.convertButton.pack(side=tk.BOTTOM, padx=default_padx, pady=default_pady)
 
-    # Add content to the other frame
-    otherLabel = tkinter.Label(otherSettingsFrame, text="Var name:")
-    otherLabel.grid(row=0, column=0, padx=default_padx, pady=default_pady)
+        self.outputSettingsFrame.pack(side=tk.LEFT, fill="both", expand=True, padx=default_padx, pady=default_pady)
+        self.otherSettingsFrame.pack(side=tk.RIGHT, fill="both", expand=True, padx=default_padx, pady=default_pady)
 
-    otherEntry = tkinter.Text(otherSettingsFrame, width=10, height=1)
-    otherEntry.grid(row=0, column=1, padx=default_padx, pady=default_pady)
+        self.outputEntry1.pack(side=tk.TOP, fill="both", expand=True)
+        self.outputLabel1.pack(side=tk.LEFT)
+        self.outputText1.pack(side=tk.LEFT, fill="x", expand=True, padx=default_padx)
 
-    otherLabel = tkinter.Label(otherSettingsFrame, text="Var name:")
-    otherLabel.grid(row=1, column=0, padx=default_padx, pady=default_pady)
+        self.outputEntry2.pack(side=tk.TOP, fill="both", expand=True)
+        self.outputLabel2.pack(side=tk.LEFT)
+        self.outputText2.pack(side=tk.LEFT, fill="x", expand=True, padx=default_padx)
 
-    otherEntry = tkinter.Text(otherSettingsFrame, width=10, height=1)
-    otherEntry.grid(row=1, column=1, padx=default_padx, pady=default_pady)
+        self.outputEntry3.pack(side=tk.TOP, fill="both", expand=True)
+        self.outputLabel3.pack(side=tk.LEFT)
+        self.outputText3.pack(side=tk.LEFT, fill="x", expand=True, padx=default_padx)
 
-    # Main Loop
-    root.mainloop()
-
+        self.outputEntry4.pack(side=tk.TOP, fill="both", expand=True)
+        self.outputLabel4.pack(side=tk.LEFT)
+        self.outputText4.pack(side=tk.LEFT, fill="x", expand=True, padx=default_padx)
 
 # Function to start the program.
 def main():
-    create_user_interface()
+    root = tk.Tk()
+    MainApplication(root)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
