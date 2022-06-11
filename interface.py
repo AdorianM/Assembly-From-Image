08b3290876edcd9c082f3f1a718921332e269c67
Adorian Mititean <Adorian.Mititean@student.utcluj.ro>
2022-06-11 10:53:33 +0300

    updated to full class-based

--- interface.py
+++ interface.py
@@ -4,6 +4,44 @@ from PIL import Image, ImageTk
 default_padx = 10
 default_pady = 5
 
+class ImageLabel(tk.Label):
+    def __init__(self, parent, imgPath, *args, **kwargs):
+        tk.Label.__init__(self, parent, *args, **kwargs)
+        self.image = ImageTk.PhotoImage(Image.open(imgPath))
+        self.draw()
+
+    def draw(self):
+        self.configure(image=self.image)
+
+class LabeledText(tk.Frame):
+    def __init__(self, parent, label, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.label = tk.Label(self, text=label)
+        self.text = tk.Text(self,  height=1, width=5)
+
+        self.label.pack(side="left")
+        self.text.pack(side="left", fill='x', expand=True, padx=default_padx)
+
+class SettingsFrame(tk.LabelFrame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.LabelFrame.__init__(self, parent, *args, **kwargs)
+
+        self.outputSettingsFrame = tk.LabelFrame(self, text="Output Settings", bg="#EFEFEA")
+        self.otherSettingsFrame = tk.LabelFrame(self, text="Other Settings", bg="#EFEFEA")
+
+        self.labeledText1 = LabeledText(self.outputSettingsFrame, "Output File Name:")
+        self.labeledText2 = LabeledText(self.outputSettingsFrame, "Output File Name:")
+        self.labeledText3 = LabeledText(self.otherSettingsFrame, "Output File Name:")
+        self.labeledText4 = LabeledText(self.otherSettingsFrame, "Output File Name:")
+
+        self.outputSettingsFrame.pack(side=tk.LEFT, fill="both", expand=True, padx=default_padx, pady=default_pady)
+        self.otherSettingsFrame.pack(side=tk.RIGHT, fill="both", expand=True, padx=default_padx, pady=default_pady)
+
+        self.labeledText1.pack(side=tk.TOP, fill="both", expand=True)
+        self.labeledText2.pack(side=tk.TOP, fill="both", expand=True)
+        self.labeledText3.pack(side=tk.TOP, fill="both", expand=True)
+        self.labeledText4.pack(side=tk.TOP, fill="both", expand=True)
+
 class MainApplication(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
@@ -17,29 +55,9 @@ class MainApplication(tk.Frame):
 
         baseFrame = tk.Frame(self.parent, borderwidth=14, bg="#EFEFEA")
 
-        self.logo = ImageTk.PhotoImage(Image.open("assets/chef.ico"))
-        self.logoLabel = tk.Label(baseFrame, image=self.logo)
-        self.settingFrame = tk.LabelFrame(baseFrame, text="Settings Panel", bg="#EFEFEA")
+        self.logoLabel = ImageLabel(baseFrame, "assets/chef.ico")
+        self.settingFrame = SettingsFrame(baseFrame, text="Settings Panel", bg="#EFEFEA")
         self.convertButton = tk.Button(baseFrame, text="Convert", command=())
-
-        self.outputSettingsFrame = tk.LabelFrame(self.settingFrame, text="Output Settings", bg="#EFEFEA")
-        self.otherSettingsFrame = tk.LabelFrame(self.settingFrame, text="Other Settings", bg="#EFEFEA")
-
-        self.outputEntry1 = tk.Frame(self.outputSettingsFrame, bg="#EFEFEA")
-        self.outputLabel1 = tk.Label(self.outputEntry1, text="Output File Name:")
-        self.outputText1 = tk.Text(self.outputEntry1, height=1, width=5)
-
-        self.outputEntry2 = tk.Frame(self.outputSettingsFrame, bg="#EFEFEA")
-        self.outputLabel2 = tk.Label(self.outputEntry2, text="Output File Name:")
-        self.outputText2 = tk.Text(self.outputEntry2, height=1, width=5)
-
-        self.outputEntry3 = tk.Frame(self.otherSettingsFrame, bg="#EFEFEA")
-        self.outputLabel3 = tk.Label(self.outputEntry3, text="Output File Name:")
-        self.outputText3 = tk.Text(self.outputEntry3, height=1, width=5)
-
-        self.outputEntry4 = tk.Frame(self.otherSettingsFrame, bg="#EFEFEA")
-        self.outputLabel4 = tk.Label(self.outputEntry4, text="Output File Name:")
-        self.outputText4 = tk.Text(self.outputEntry4, height=1, width=5)
 
         baseFrame.pack(fill="both", expand=True)
 
@@ -47,26 +65,6 @@ class MainApplication(tk.Frame):
         self.settingFrame.pack(side=tk.TOP, fill="both", expand=True, padx=default_padx, pady=default_pady)
         self.convertButton.pack(side=tk.BOTTOM, padx=default_padx, pady=default_pady)
 
-        self.outputSettingsFrame.pack(side=tk.LEFT, fill="both", expand=True, padx=default_padx, pady=default_pady)
-        self.otherSettingsFrame.pack(side=tk.RIGHT, fill="both", expand=True, padx=default_padx, pady=default_pady)
-
-        self.outputEntry1.pack(side=tk.TOP, fill="both", expand=True)
-        self.outputLabel1.pack(side=tk.LEFT)
-        self.outputText1.pack(side=tk.LEFT, fill="x", expand=True, padx=default_padx)
-
-        self.outputEntry2.pack(side=tk.TOP, fill="both", expand=True)
-        self.outputLabel2.pack(side=tk.LEFT)
-        self.outputText2.pack(side=tk.LEFT, fill="x", expand=True, padx=default_padx)
-
-        self.outputEntry3.pack(side=tk.TOP, fill="both", expand=True)
-        self.outputLabel3.pack(side=tk.LEFT)
-        self.outputText3.pack(side=tk.LEFT, fill="x", expand=True, padx=default_padx)
-
-        self.outputEntry4.pack(side=tk.TOP, fill="both", expand=True)
-        self.outputLabel4.pack(side=tk.LEFT)
-        self.outputText4.pack(side=tk.LEFT, fill="x", expand=True, padx=default_padx)
-
-# Function to start the program.
 def main():
     root = tk.Tk()
     MainApplication(root)
